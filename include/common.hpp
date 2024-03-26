5e349fdc8c401d8b8af379218667c8393f66e1c7
Kelbon Nik <kelbonage@gmail.com>
2024-03-26 12:33:15 +0400

    fix hardware interferece values

--- include/common.hpp
+++ include/common.hpp
@@ -11,13 +11,8 @@
 
 namespace dd {
 
-#ifdef __cpp_lib_hardware_interference_size
-using std::hardware_constructive_interference_size;
-using std::hardware_destructive_interference_size;
-#else
 constexpr std::size_t hardware_constructive_interference_size = 64;
 constexpr std::size_t hardware_destructive_interference_size = 64;
-#endif
 
 struct not_movable {
   constexpr not_movable() noexcept = default;
