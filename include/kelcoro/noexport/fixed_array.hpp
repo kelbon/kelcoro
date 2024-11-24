74ad839be7f56b4a470cff65ff912c200f52cc83
Андрей Будиловский <a.budilovskiy@ideco.ru>
2024-11-24 18:29:42 +0000

    safety

--- include/kelcoro/noexport/fixed_array.hpp
+++ include/kelcoro/noexport/fixed_array.hpp
@@ -3,20 +3,24 @@
 #include <cstddef>
 #include <memory>
 #include <memory_resource>
+#include <type_traits>
+#include <utility>
 
 #include "kelcoro/common.hpp"
 
 namespace dd::noexport {
+
 template <typename T>
 struct fixed_array {
  private:
   T* arr = nullptr;
-  std::size_t n = 0;
+  size_t n = 0;
   std::pmr::memory_resource* resourse = std::pmr::new_delete_resource();
 
  public:
+  static_assert(std::is_same_v<T, std::decay_t<T>>);
   static auto default_factory() {
-    return [](std::size_t) { return T{}; };
+    return [](size_t) { return T{}; };
   }
   fixed_array() = default;
   fixed_array(fixed_array&& rhs) noexcept {
@@ -27,14 +31,14 @@ struct fixed_array {
     std::swap(n, rhs.n);
     std::swap(resourse, rhs.resourse);
   }
-  fixed_array(std::size_t n, std::pmr::memory_resource& resource = *std::pmr::new_delete_resource())
+  fixed_array(size_t n, std::pmr::memory_resource& resource = *std::pmr::new_delete_resource())
       : fixed_array(n, default_factory(), resource) {
   }
-  fixed_array(std::size_t n, std::invocable<std::size_t> auto&& factory,
+  fixed_array(size_t n, std::invocable<size_t> auto&& factory,
               std::pmr::memory_resource& resource = *std::pmr::new_delete_resource()) noexcept
       : n(n), resourse(&resource) {
     arr = (T*)resource.allocate(sizeof(T) * n, alignof(T));
-    std::size_t constructed = 0;
+    size_t constructed = 0;
     bool fail = true;
     scope_exit _{[&] {
       if (fail) {
@@ -42,10 +46,8 @@ struct fixed_array {
         resourse->deallocate(arr, sizeof(T) * n, alignof(T));
       }
     }};
-    for (; constructed < n; constructed++) {
-      T* ptr = &arr[constructed];
-      new (ptr) T(factory(constructed));
-    }
+    for (; constructed < n; constructed++)
+      new (arr + constructed) T(factory(constructed));
     fail = false;
   }
 
@@ -55,23 +57,26 @@ struct fixed_array {
   }
 
   T* data() {
-    return &arr[0];
+    return arr;
   }
 
   T* data() const {
-    return &arr[0];
+    return arr;
   }
 
   void reset() noexcept {
-    if (arr) {
-      std::destroy_n(arr, n);
-      resourse->deallocate(arr, sizeof(T) * n, alignof(T));
-      arr = nullptr;
-      n = 0;
-    }
+    if (!arr)
+      return;
+    // If someone somehow thought of calling reset on array in their destructor
+    auto moved = std::move(*this);
+    std::destroy_n(moved.arr, moved.n);
+    // assume noexcept
+    moved.resourse->deallocate(moved.arr, sizeof(T) * moved.n, alignof(T));
+    moved.arr = nullptr;
+    moved.n = 0;
   }
 
-  std::size_t size() const noexcept {
+  size_t size() const noexcept {
     return n;
   }
 
@@ -91,13 +96,13 @@ struct fixed_array {
     return arr + n;
   }
 
-  T& operator[](std::size_t i) noexcept {
+  T& operator[](size_t i) noexcept {
     assert(arr);
     assert(i < n);
     return arr[i];
   }
 
-  const T& operator[](std::size_t i) const noexcept {
+  const T& operator[](size_t i) const noexcept {
     assert(arr);
     assert(i < n);
     return arr[i];
@@ -107,4 +112,5 @@ struct fixed_array {
     reset();
   }
 };
+
 }  // namespace dd::noexport
